7c9e1df83ccfd9ccbc7d4c74f4f4b23ddcd8336a
arjit88 <arjitanand88@gmail.com>
2024-09-15 00:33:16 +0530

    Make the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="bg-[#F6F8FC] h-screen w-screen overflow-hidden">
       <NavBar />
+      <Sidebar />
     </div>
   );
 }
